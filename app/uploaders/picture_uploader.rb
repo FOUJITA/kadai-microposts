7c5d475746f8be56b923695804b31b0ddc6742be
norikazu.fujita <efup.maker201@gmail.com>
2018-08-15 13:01:42 +0000

    debug reset resize

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -2,7 +2,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   #Include RMagick or MiniMagick support: #default:commented
   include CarrierWave::RMagick
   #include CarrierWave::MiniMagick
-  process resize_to_limit: [300, 300]
+  #process resize_to_limit: [300, 300]
   # Choose what kind of storage to use for this uploader:
   #storage :file#default:commented
   #storage :fog
